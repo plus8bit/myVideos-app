83a36bbccbe7d6b8542c58cf3c93bf5c823ac27d
Nick <plus0bit@gmail.com>
2019-04-28 19:47:36 +0300

    Initial xcode project

---
+++ MyVideos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyVideos
+//
+//  Created by Nick on 28/04/2019.
+//  Copyright © 2019 Nick Danilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
